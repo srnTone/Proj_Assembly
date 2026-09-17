e96d7443b35d29dec3ccd20b02a89b6988139586
srnTone <68011813@kmitl.ac.th>
2026-09-17 14:39:08 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -5,25 +5,21 @@ set IRVINE_DIR=C:\Irvine
 if exist main.exe del /f /q main.exe
 
 echo [1/2] Assembling all modules...
-%MASM_BIN%\ml.exe /c /coff /I"%IRVINE_DIR%" main.asm key_schedule.asm des_engine.asm dumper.asm file_io.asm parser.asm
+%MASM_BIN%\ml.exe /c /coff /Cp /I"%IRVINE_DIR%" main.asm key_schedule.asm des_engine.asm dumper.asm file_io.asm parser.asm
 if errorlevel 1 (
     echo [ERROR] Assembly failed!
     pause
     exit /b %errorlevel%
 )
 
-echo [2/2] Linking executable...
-%MASM_BIN%\link.exe /SUBSYSTEM:CONSOLE /LIBPATH:"%IRVINE_DIR%" /LIBPATH:"C:\masm32\lib" main.obj key_schedule.obj des_engine.obj dumper.obj file_io.obj parser.obj Irvine32.lib kernel32.lib user32.lib /OUT:main.exe
+echo [2/2] Linking executable with modern security flags...
+%MASM_BIN%\link.exe /SUBSYSTEM:CONSOLE /DYNAMICBASE /NXCOMPAT /LIBPATH:"%IRVINE_DIR%" /LIBPATH:"C:\masm32\lib" main.obj key_schedule.obj des_engine.obj dumper.obj file_io.obj parser.obj Irvine32.lib kernel32.lib user32.lib /OUT:main.exe
 if errorlevel 1 (
     echo [ERROR] Linking failed!
     pause
     exit /b %errorlevel%
 )
 
-REM ปลดบล็อก Zone Identifier อัตโนมัติ
-powershell -Command "Unblock-File -Path .\main.exe"
-
-echo [SUCCESS] Build complete! Running shell...
-echo -------------------------------------------------
-.\main.exe
+echo [SUCCESS] Build complete!
+main.exe
 pause
